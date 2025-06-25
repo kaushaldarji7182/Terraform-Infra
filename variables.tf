10260926b8bd6caead4977c5e1665e4d618198ae
Kaushal Darji <64036351+kaushaldarji7182@users.noreply.github.com>
2025-06-25 23:07:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "vpc_cidr"            { default = "10.0.0.0/16" }
-variable "public_subnet_1"     { default = "10.0.1.0/24" }
-variable "public_subnet_2"     { default = "10.0.2.0/24" }
-variable "private_subnet_1"    { default = "10.0.3.0/24" }
-variable "private_subnet_2"    { default = "10.0.4.0/24" }
-variable "eks_cluster_name"    { default = "terraform-cluster" }
+variable "vpc_cidr"            { default = "10.1.0.0/16" }
+variable "public_subnet_1"     { default = "10.1.1.0/24" }
+variable "public_subnet_2"     { default = "10.1.2.0/24" }
+variable "private_subnet_1"    { default = "10.1.3.0/24" }
+variable "private_subnet_2"    { default = "10.1.4.0/24" }
+variable "eks_cluster_name"    { default = "kaushal-cluster" }
 variable "eks_desired_size"    { default = 2 }
 variable "eks_max_size"        { default = 3 }
 variable "eks_min_size"        { default = 1 }
