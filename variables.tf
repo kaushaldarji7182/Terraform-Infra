9668b50687bd97f10fe5deb65d6bdc9e71be6fa6
Kaushal Darji <64036351+kaushaldarji7182@users.noreply.github.com>
2025-06-26 00:20:20 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,9 @@ variable "public_subnet_1"     { default = "10.0.1.0/24" }
 variable "public_subnet_2"     { default = "10.0.2.0/24" }
 variable "private_subnet_1"    { default = "10.0.3.0/24" }
 variable "private_subnet_2"    { default = "10.0.4.0/24" }
+variable "region" {
+  default = "us-west-1"
+}
 
 variable "eks_desired_size"    { default = 2 }
 variable "eks_max_size"        { default = 3 }
