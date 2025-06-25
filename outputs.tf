a300d8c636359767759c7da340ffd6c01432e20a
Kaushal Darji <64036351+kaushaldarji7182@users.noreply.github.com>
2025-06-25 23:06:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,11 @@
-
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
 output "rds_endpoint" {
-  value = aws_db_instance.rds.endpoint
+  value = aws_db_instance.kaushal_rds.endpoint
 }
 
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.cart.name
+  value = aws_dynamodb_table.kaushal_cart.name
 }
